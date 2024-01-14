1c65e5c114e1c168141f511230d33cd76745df53
Shehzer00 <154761350+Shehzer00@users.noreply.github.com>
2024-01-14 23:19:49 +0500

    Add files via upload

---
+++ partA.asm
@@ -0,0 +1,28 @@
+[org 100h]
+
+rev:
+reverse:
+push bp
+mov bp, sp
+mov bx, 0
+l1:
+mov ax,[bp+4]
+pop ax
+add bx, 2
+cmp bx, 12
+jne l1
+pop bp
+ret 10
+
+
+start:
+mov cx, 10
+rev_loop:
+mov ah, 1
+int 21h
+mov dl, al
+push dx
+loop rev_loop
+call rev
+mov ah, 4ch
+int 21h
